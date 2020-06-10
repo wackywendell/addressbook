da6a9217ba3abf0cd4c520b287baf042e8a20d2b
Wendell Smith <wackywendell@gmail.com>
2020-06-09 21:26:18 -0400

    Add create_book to owned version

--- src/owned.rs
+++ src/owned.rs
@@ -26,3 +26,35 @@ impl AddressBook {
         &self.contacts[..]
     }
 }
+
+pub fn create_book() -> AddressBook {
+    let mut book = AddressBook::new();
+
+    let alice = Contact {
+        name: "Alice".to_owned(),
+        address: "123 Apple Ave".to_owned(),
+    };
+
+    // Add this contact to the book
+    book.add(alice);
+
+    // Add another contact to the book
+    let bob = Contact {
+        name: "Bob".to_owned(),
+        address: "123 Berry Boardwalk".to_owned(),
+    };
+
+    // Copy this contact into the array of contacts
+    book.add(bob);
+
+    // Add a third contact to the book
+    let carol = Contact {
+        name: "Carol".to_owned(),
+        address: "123 Cherry Crossing".to_owned(),
+    };
+
+    // Copy this contact into the array of contacts
+    book.add(carol);
+
+    book
+}
